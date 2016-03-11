fbdde536745049d6c1455573e9c855dfb0b079d1
Roy Li <xiaolongli1988@gmail.com>
2016-03-11 12:22:05 -0500

    finished part 2 --again

--- SpringProject/src/main/java/com/example/SpringProjectApplication.java
+++ SpringProject/src/main/java/com/example/SpringProjectApplication.java
@@ -1,11 +1,20 @@
 package com.example;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class SpringProjectApplication {
 
+	@RequestMapping("/user")
+	  public Principal user(Principal user) {
+	    return user;
+	  }
+	
 	 @RequestMapping("/resource")
 	  public Map<String,Object> home() {
 	    Map<String,Object> model = new HashMap<String,Object>();
@@ -20,9 +34,31 @@ public class SpringProjectApplication {
 	    model.put("content", "Hello World");
 	    return model;
 	  }
+	 
+	 @Configuration
+	 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	 protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+			@Override
+			protected void configure(HttpSecurity http) throws Exception {
+				http.httpBasic()
+						.and()
+						.authorizeRequests()
+						.antMatchers("/index.html", "/home.html", "/login.html",
+								"/").permitAll().anyRequest().authenticated().and()
+						.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+						.csrf().csrfTokenRepository(csrfTokenRepository());
+			}
 	
+	 private CsrfTokenRepository csrfTokenRepository() {
+		  HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		  repository.setHeaderName("X-XSRF-TOKEN");
+		  return repository;
+		}
+	 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProjectApplication.class, args);
 	}
+}
+	 
 }
