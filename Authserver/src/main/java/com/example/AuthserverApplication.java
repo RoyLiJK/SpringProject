9f0a5d99e868ccafad3df8ee0aba4eb44e6a85d3
ming42889 <ming42889@hotmail.com>
2016-03-12 19:51:54 -0500

    part5

--- Authserver/src/main/java/com/example/AuthserverApplication.java
+++ Authserver/src/main/java/com/example/AuthserverApplication.java
@@ -5,12 +5,15 @@ import java.security.Principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAuthorizationServer
-public class AuthserverApplication extends WebMvcConfigurerAdapter {
+@RestController
+@EnableResourceServer
+public class AuthserverApplication {
 
 	@RequestMapping("/user")
 	  public Principal user(Principal user) {
--- Resources/src/main/java/com/example/ResourcesApplication.java
+++ Resources/src/main/java/com/example/ResourcesApplication.java
@@ -6,24 +6,18 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@SpringBootApplication
 @RestController
 @EnableResourceServer
-@SpringBootApplication
-@EnableWebSecurity
-public class ResourcesApplication extends WebSecurityConfigurerAdapter {
+public class ResourcesApplication {
 	
 	
-	@RequestMapping("/resource")
+	@RequestMapping("/")
 	  public Map<String,Object> home() {
 	    Map<String,Object> model = new HashMap<String,Object>();
 	    model.put("id", UUID.randomUUID().toString());
@@ -34,18 +28,4 @@ public class ResourcesApplication extends WebSecurityConfigurerAdapter {
 	public static void main(String[] args) {
 		SpringApplication.run(ResourcesApplication.class, args);
 	}
-	
-	/*
-	@Bean
-	  HeaderHttpSessionStrategy sessionStrategy() {
-	    return new HeaderHttpSessionStrategy();
-	  }
-	  
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic().disable();
-		http.authorizeRequests().anyRequest().authenticated();
-	}
-	*/
 }
